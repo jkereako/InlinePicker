fd016469ff97306d2a576799ea01b7f1b3e504e2
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-04-21 16:42:25 -0400

    Add test for cellForRowAtIndexPath

--- Tests/UnitTests/TableViewControllerTests.swift
+++ Tests/UnitTests/TableViewControllerTests.swift
@@ -11,9 +11,16 @@ import XCTest
 
 final class TableViewControllerTests: XCTestCase {
   private var viewController: TableViewController!
+  private let dataSource: [CellModelType] = [
+    SubtitleCellModel(rowIndex: 0, title: "Row 1", subTitle: "subtitle 1"),
+    SubtitleCellModel(rowIndex: 1, title: "Row 2", subTitle: "subtitle 2"),
+    SubtitleCellModel(rowIndex: 2, title: "Row 3", subTitle: "subtitle 3"),
+    SubtitleCellModel(rowIndex: 3, title: "Row 4", subTitle: "subtitle 4"),
+    SubtitleCellModel(rowIndex: 4, title: "Row 5", subTitle: "subtitle 5")
+  ]
 
   override func setUp() {
-    viewController = TableViewController(style: .Plain, dataSource: TableViewDataSource.dataSource)
+    viewController = TableViewController(style: .Plain, dataSource: dataSource)
   }
 
   override func tearDown() {
@@ -25,9 +32,19 @@ final class TableViewControllerTests: XCTestCase {
   }
 
   func testNumberOfRowsInSection() {
+    let section = 0
     XCTAssertEqual(
-      viewController.tableView(viewController.tableView, numberOfRowsInSection: 0),
-      TableViewDataSource.dataSource.count
+      viewController.tableView(viewController.tableView, numberOfRowsInSection: section),
+      dataSource.count
     )
+  }
+
+  func testTableViewRowDequeue() {
+    viewController.tableView.registerClass(SubtitleCell.self, forCellReuseIdentifier: "subtitle")
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    let cell = viewController.tableView(viewController.tableView, cellForRowAtIndexPath: indexPath)
+
+    XCTAssertNotNil(cell)
+    XCTAssertTrue(cell is SubtitleCell)
   }
 }
