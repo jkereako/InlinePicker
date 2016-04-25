a65dd5b08a72c97a40f0a581ddfd875be8b92375
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-04-24 20:45:54 -0400

    Add some tests

--- Tests/UnitTests/TableViewControllerTests.swift
+++ Tests/UnitTests/TableViewControllerTests.swift
@@ -10,23 +10,40 @@ import XCTest
 @testable import InlinePicker
 
 final class TableViewControllerTests: XCTestCase {
+  private let storyboardName = "Main"
+  private let viewControllerStoryboardIdentifier = "tableViewController"
+  private var storyboard: UIStoryboard!
   private var sut: TableViewController!
   private let dataSource: [CellModelType] = [
     SubtitleCellModel(rowIndex: 0, title: "Row 1", subTitle: "subtitle 1"),
     SubtitleCellModel(rowIndex: 1, title: "Row 2", subTitle: "subtitle 2"),
     PickerViewCellModel(rowIndex: 2),
     SubtitleCellModel(rowIndex: 3, title: "Row 4", subTitle: "subtitle 4"),
-    SubtitleCellModel(rowIndex: 4, title: "Row 5", subTitle: "subtitle 5")
+    SubtitleCellModel(rowIndex: 4, title: "Row 5", subTitle: "subtitle 5"),
+    SubtitleCellModel(rowIndex: 5, title: "Row 5", subTitle: "subtitle 6")
   ]
 
   override func setUp() {
-    sut = TableViewController(style: .Plain, dataSource: TableViewDataSource())
+    storyboard = UIStoryboard(name: storyboardName, bundle: NSBundle.mainBundle())
+    sut = storyboard.instantiateViewControllerWithIdentifier(viewControllerStoryboardIdentifier) as?
+      TableViewController
   }
 
   override func tearDown() {
     sut = nil
   }
-  
+
+  func testStoryboardIsNotNil() {
+    XCTAssertNotNil(UIStoryboard(name: storyboardName, bundle: NSBundle.mainBundle()))
+  }
+
+  func testViewControllerIsNotNil() {
+    let storyboard = UIStoryboard(name: storyboardName, bundle: NSBundle.mainBundle())
+    XCTAssertNotNil(
+      storyboard.instantiateViewControllerWithIdentifier(viewControllerStoryboardIdentifier)
+    )
+  }
+
   func testNumberOfSections() {
     XCTAssertEqual(sut.numberOfSectionsInTableView(sut.tableView), 1)
   }
@@ -48,14 +65,14 @@ final class TableViewControllerTests: XCTestCase {
     XCTAssertTrue(cell is SubtitleCell)
   }
   /*
-  // This crashes because there the picker view is an IBOutlet and it is nil.
-  func testTableViewDequeuesPickerRow() {
-    viewController.tableView.registerClass(PickerCell.self, forCellReuseIdentifier: "picker")
-    let indexPath = NSIndexPath(forRow: 2, inSection: 0)
-    let cell = viewController.tableView(viewController.tableView, cellForRowAtIndexPath: indexPath)
+   // This crashes because there the picker view is an IBOutlet and it is nil.
+   func testTableViewDequeuesPickerRow() {
+   viewController.tableView.registerClass(PickerCell.self, forCellReuseIdentifier: "picker")
+   let indexPath = NSIndexPath(forRow: 2, inSection: 0)
+   let cell = viewController.tableView(viewController.tableView, cellForRowAtIndexPath: indexPath)
 
-    XCTAssertNotNil(cell)
-    XCTAssertTrue(cell is PickerCell)
-  }
-  */
+   XCTAssertNotNil(cell)
+   XCTAssertTrue(cell is PickerCell)
+   }
+   */
 }
